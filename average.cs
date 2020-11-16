8d28cb4a2af6dcee8dff206483173a534de73f74
Nityananda <nityanandalhn@gmail.com>
2020-11-16 17:44:05 +0100

    Cleaned average.cs

--- average.cs
+++ average.cs
@@ -1,20 +1,30 @@
+/* 
+The following code is free code void of copyright,
+it's purpose is obtaining the average of a series of numbers
+ */
 using System;
-public class Average{
-public static void Main(){
-double a=0, b, c=0, d;
-string s;
-Console.Write("Insert a mark: ");
-s=Console.ReadLine();	// we read the mark as string to check the word "end"	 
-while(s!="end"){  // if the mark isn't an end then we pass it to double to work with it
-b=Convert.ToDouble(s);
-c++;   //counter of numbers
-a+=b;  // sum of all the notes
-Console.Write("Insert a mark: ");
-s=Console.ReadLine();
-}
-if(c!=0){
-	d=a/c;  //calculate the average
-Console.WriteLine("The average is {0}.",d);
-}
-}
+public class Average
+{
+	public static void Main()
+	{
+		double sum = 0, note = 0, counter = 0, average = 0;
+		string mark = " ";
+		Console.Write("Insert a mark (\"end\" to finish): ");
+		mark = Console.ReadLine();	 
+		while(mark != "end")
+		{
+			/* This part can crash the application, it could be improved by adding 
+				error control methods to the string to double conversion */
+			note = Convert.ToDouble(mark);
+			counter++;
+			sum += note;
+			Console.Write("Insert a mark (\"end\" to finish): ");
+			mark = Console.ReadLine();
+		}
+		if(counter != 0)
+		{
+		average = sum / counter;
+		Console.WriteLine("The average is {0}.", average);
+		}
+	}
 }
